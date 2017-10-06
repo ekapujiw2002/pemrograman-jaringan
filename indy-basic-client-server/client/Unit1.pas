d7d0bc1be57225b23e0c1703a3f6cf5dea18d7fe
Eka Puji Widiyanto <ekapujiw2002@gmail.com>
2017-10-06 17:02:35 +0700

    Indy Client Demo
    Send, read, connect

--- indy-basic-client-server/client/Unit1.pas
+++ indy-basic-client-server/client/Unit1.pas
@@ -5,14 +5,23 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, Buttons, IdBaseComponent, IdComponent,
-  IdTCPConnection, IdTCPClient;
+  IdTCPConnection, IdTCPClient, ExtCtrls;
 
 type
   TForm1 = class(TForm)
     idtcpclnt1: TIdTCPClient;
     btn1: TBitBtn;
     btn2: TBitBtn;
+    tmr1: TTimer;
+    lbledt1: TLabeledEdit;
+    btn3: TBitBtn;
+    mmo1: TMemo;
+    tmr2: TTimer;
     procedure btn1Click(Sender: TObject);
+    procedure tmr1Timer(Sender: TObject);
+    procedure btn2Click(Sender: TObject);
+    procedure btn3Click(Sender: TObject);
+    procedure tmr2Timer(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,15 +40,70 @@ begin
   try
     with idtcpclnt1 do
     begin
-      Host := '127.0.0.1';
-      Port := 30000;
-      Connect;
+      //      Host := '127.0.0.1';
+      //      Port := 30000;
+      //      Connect;
+      ConnectTimeout := 5000;
+      Connect('127.0.0.1', 30000);
+      if Connected then
+      begin
+        ShowMessage('koneksi ok');
+      end;
     end;
   except
     on err: Exception do
     begin
 
     end;
+  end;
+end;
+
+procedure TForm1.tmr1Timer(Sender: TObject);
+begin
+  try
+    Form1.Caption := 'Client : ' + FormatDateTime('dd-mm-yyyy hh:nn:ss.zzz',
+      Now);
+  except
+
+  end;
+end;
+
+procedure TForm1.btn2Click(Sender: TObject);
+begin
+  try
+    idtcpclnt1.Disconnect;
+  except
+
+  end;
+end;
+
+procedure TForm1.btn3Click(Sender: TObject);
+begin
+  try
+    idtcpclnt1.IOHandler.Write(lbledt1.Text);
+  except
+
+  end;
+end;
+
+procedure TForm1.tmr2Timer(Sender: TObject);
+var
+  s1: string;
+begin
+  try
+    with idtcpclnt1 do
+    begin
+      if Connected then
+      begin
+        if not IOHandler.InputBufferIsEmpty then
+        begin
+          s1 := IOHandler.InputBufferAsString();
+          mmo1.Lines.Add(s1);
+        end;
+      end;
+    end;
+  except
+
   end;
 end;
 
--- indy-basic-client-server/client/Unit1.~dfm
+++ indy-basic-client-server/client/Unit1.~dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
   Left = 192
   Top = 153
-  Width = 696
-  Height = 480
+  Width = 319
+  Height = 283
   Caption = 'Client'
   Color = clBtnFace
   Font.Charset = OEM_CHARSET
@@ -12,6 +12,9 @@ object Form1: TForm1
   Font.Style = []
   OldCreateOrder = False
   Position = poScreenCenter
+  DesignSize = (
+    311
+    256)
   PixelsPerInch = 96
   TextHeight = 18
   object btn1: TBitBtn
@@ -30,6 +33,36 @@ object Form1: TForm1
     Height = 25
     Caption = 'DISCONNECT'
     TabOrder = 1
+    OnClick = btn2Click
+  end
+  object lbledt1: TLabeledEdit
+    Left = 8
+    Top = 56
+    Width = 201
+    Height = 26
+    EditLabel.Width = 40
+    EditLabel.Height = 18
+    EditLabel.Caption = 'DATA'
+    TabOrder = 2
+  end
+  object btn3: TBitBtn
+    Left = 8
+    Top = 88
+    Width = 75
+    Height = 25
+    Caption = 'SEND'
+    TabOrder = 3
+    OnClick = btn3Click
+  end
+  object mmo1: TMemo
+    Left = 8
+    Top = 120
+    Width = 297
+    Height = 129
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ReadOnly = True
+    ScrollBars = ssVertical
+    TabOrder = 4
   end
   object idtcpclnt1: TIdTCPClient
     ConnectTimeout = 0
@@ -38,5 +71,16 @@ object Form1: TForm1
     ReadTimeout = -1
     Left = 64
     Top = 80
+  end
+  object tmr1: TTimer
+    OnTimer = tmr1Timer
+    Left = 96
+    Top = 88
+  end
+  object tmr2: TTimer
+    Interval = 100
+    OnTimer = tmr2Timer
+    Left = 128
+    Top = 96
   end
 end
